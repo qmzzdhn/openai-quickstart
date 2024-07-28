91e452f708e9646dbf45d7d5f5f8d549086e365d
Charlie Qiu <qiuch3@oocl.com>
2024-07-28 20:16:27 +0800

    use of AI translator

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -18,10 +18,13 @@ if __name__ == "__main__":
     api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
     model = OpenAIModel(model=model_name, api_key=api_key)
 
-
     pdf_file_path = args.book if args.book else config['common']['book']
     file_format = args.file_format if args.file_format else config['common']['file_format']
 
+    origin_lang = args.origin_language if args.origin_language else config['common']['origin_language']
+    target_lang = args.target_language if args.target_language else config['common']['target_language']
+    output_file_path = args.output_file if args.output_file else config['common']['output_file']
+
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    translator.translate_pdf(pdf_file_path, file_format, origin_lang, target_lang, output_file_path)
--- openai-translator/ai_translator/translator/pdf_translator.py
+++ openai-translator/ai_translator/translator/pdf_translator.py
@@ -4,18 +4,19 @@ from translator.pdf_parser import PDFParser
 from translator.writer import Writer
 from utils import LOG
 
+
 class PDFTranslator:
     def __init__(self, model: Model):
         self.model = model
         self.pdf_parser = PDFParser()
         self.writer = Writer()
 
-    def translate_pdf(self, pdf_file_path: str, file_format: str = 'PDF', target_language: str = '中文', output_file_path: str = None, pages: Optional[int] = None):
+    def translate_pdf(self, pdf_file_path: str, file_format: str = 'PDF', origin_language: str = None, target_language: str = None, output_file_path: str = None, pages: Optional[int] = None):
         self.book = self.pdf_parser.parse_pdf(pdf_file_path, pages)
 
         for page_idx, page in enumerate(self.book.pages):
             for content_idx, content in enumerate(page.contents):
-                prompt = self.model.translate_prompt(content, target_language)
+                prompt = self.model.translate_prompt(content, origin_language, target_language)
                 LOG.debug(prompt)
                 translation, status = self.model.make_request(prompt)
                 LOG.info(translation)
